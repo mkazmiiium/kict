a2f669def9a16b2404f346c8dfa1d6b7c1988131
mkazmiiium <mkazmi@live.iium.edu.my>
2022-12-16 06:24:42 +0800

    after format macbook

--- resources/views/pgcentralised/view.blade.php
+++ resources/views/pgcentralised/view.blade.php
@@ -38,7 +38,7 @@
 
         <div class="col-md2">
                 {{-- <button type="button" class="btn btn-block btn-primary btn-lg">Primary</button> --}}
-                <a href="/pg_add_booking_centralise_exam" class="btn btn-block btn-primary btn-lg">Add New PG Booking</a>
+                <a href="/pg_add_booking_centralise_exam" class="btn disabled btn-block btn-primary btn-lg">Add New PG Booking</a>
         </div>
 
         <p></p>
