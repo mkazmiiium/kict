e114ef5b7fcec184f414c9c5cbe50655135c21e0
mkazmiiium <mkazmi@live.iium.edu.my>
2023-02-16 07:48:23 +0800

    home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,7 @@
         <div class="container-fluid">
           <div class="row mb-2">
             <div class="col-sm-6">
-              <h1 class="m-0">Dashboard System for <strong>Semester 2 2022/2023</strong></h1>
+              <h1 class="m-0">Dashboard <strong>Semester 2 2022/2023</strong></h1>
             </div><!-- /.col -->
             <div class="col-sm-6">
               <ol class="breadcrumb float-sm-right">
